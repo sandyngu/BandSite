7e646aa0dc8221085bc48aea5c6460241956a31c
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-09-04 21:58:47 -0400

    Fixed Comments Button, Added Comment with JS

--- Sandy-Nguyen-sprint-two/scripts/main.js
+++ Sandy-Nguyen-sprint-two/scripts/main.js
@@ -22,20 +22,73 @@
 // commentSection.insertBefore(comment2, referenceNode);
 // commentSection.insertBefore(comment3, referenceNode);
 
-const commentsForm = document.querySelector('.comments-section__form');
-console.log('elements', commentsForm.elements);
-console.log('name element', commentsForm.elements[0]);
-console.log('by field name', commentsForm.elements.name);
+// const commentsForm = document.querySelector('.comments-section__form');
+// commentsForm.addEventListener ('submit', commentsFormHandler);
 
-function commentsFormHandler(e) {
-    e.preventDefault
-    console.log(e);
+// console.log('elements', commentsForm.elements);
+// console.log('name element', commentsForm.elements[0]);
+// console.log('by field name', commentsForm.elements.name);
+
+// function commentsFormHandler(e) {
+//     e.preventDefault;
+//     const name = e.target.name.value;
+//     const comment = e.target.comment.value;
+//     function uploadData (name, comment) {
+//         guestbookName.innerHTML = name;
+//         guestbookText.innerHTML = comment;
+//         console.log(name);
+//         console.log(comment);
+//     }
+// }
+
+let divider = document.createElement('div');
+divider.classList.add('divider', 'comments-section__guestbook-divider');
+
+let guestbookContainer = document.createElement('div');
+guestbookContainer.classList.add('comments-section__guestbook-container');
+
+let guestbookPhoto = document.createElement('div');
+guestbookPhoto.classList.add('comments-section__guestbook-photo');
+
+let guestbookEntry = document.createElement('div');
+guestbookEntry.classList.add('comments-section__guestbook-entry');
+
+let guestbookName = document.createElement('h4');
+guestbookName.classList.add('comments-section__guestbook-name');
+guestbookName.innerHTML = "Sandy Nguyen";
+
+let guestbookDate = document.createElement('div');
+guestbookDate.classList.add('comments-section__guestbook-date');
+guestbookDate.innerHTML = "04/09/2020";
+
+let guestbookText = document.createElement('div');
+guestbookText.classList.add('comments-section__guestbook-text');
+guestbookText.innerHTML = 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+
+
+/*Take data input from the comments form and upload it to the webpage as a block of content 
+Each comment should have a circle photo, the person's name, and their comment.
+Each comment will have a divider, a comments container, an entry div for the name, date and text
+The page will not refresh when the button is clicked*/
+
+let guestbook = document.querySelector('.comments-section__guestbook');
+guestbook.appendChild(divider);
+guestbook.appendChild(guestbookContainer);
+guestbookContainer.appendChild(guestbookPhoto);
+guestbookContainer.appendChild(guestbookEntry);
+guestbookEntry.appendChild(guestbookName);
+guestbookEntry.appendChild(guestbookDate);
+guestbookEntry.appendChild(guestbookText);
+
+function commentsFormHandler(event) {
+    event.preventDefault();
+    alert("The form was submitted");
     const name = e.target.name.value;
-    console.log(name);
     const comment = e.target.comment.value;
+    console.log(name);
     console.log(comment);
+    
 }
 
-commentsForm.addEventListener ('submit', commentsFormHandler);
-
-
+const commentsForm = document.querySelector('.comments-section__form');
+commentsForm.addEventListener('submit', commentsFormHandler);
