db36affe7db926f145d4dea996d419512ed32773
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-09-06 13:34:43 -0400

    JS for Comment Section Almost Complete, JS for Shows Section Done, Styling for Shows Also Complete

--- Sandy-Nguyen-sprint-two/scripts/main.js
+++ Sandy-Nguyen-sprint-two/scripts/main.js
@@ -1,4 +1,18 @@
-/*When the button of the form is pushed, the values input in the boxes will be pushed into the arrays with the values of the 3 comments*/
+const comments = [{
+    name: "Michael Lyons",
+    date: "12/12/2018",
+    comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+}, {
+    name: "Gary Wong",
+    date: "12/12/2018",
+    comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+}, {
+    name: "Theodore Duncan",
+    date: "11/15/2018",
+    comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+}]
+
+/*When the button of the form is pushed, the values input in the boxes will be pushed into the arrays with the values of the 3 comments; a new comment box will created. After submitting the form, the contents of the form will clear*/
 
 const commentsForm = document.querySelector('.comments-section__form');
 commentsForm.addEventListener('submit', commentsFormHandler);
@@ -6,25 +20,39 @@ commentsForm.addEventListener('submit', commentsFormHandler);
 function commentsFormHandler(event) {
     event.preventDefault();
     let name = event.target.name.value;
-    names.push(name);
     let comment = event.target.comment.value;
-    comments.push(comment);
+    commentsForm.reset();
+    
+    let newComment = {name:name, 
+        comment:comment};
+        comments.unshift(newComment);
 
-    function1()
-}
+        loadComment();
+    };
 
-/*Values for 3 comments put into arrays*/
+/*This creates the parent div for the comments section and loads the other comments*/
+    
+function parentFunction() {
+    let parentElement = document.createElement('div');
+    parentElement.classList.add('comments-section__guestbook');
+    let divider = document.createElement('div');
+    divider.classList.add('divider', 'comments-section__guestbook-divider');
+    let parentParent = document.querySelector('.comments-section');
+    parentParent.appendChild(parentElement);
+    parentElement.appendChild(divider);
+        
+    loadComment();
+};
 
-const guestbook = document.querySelector('.comments-section__guestbook');
-const names = ["Michael Lyons", "Gary Wong", "Theodore Duncan"];
-const dates = ["12/18/2018", "12/12/2018", "11/15/2018"];
-const comments = ["They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.", "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!", "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"];
-
-/*These functions create the comment sections*/
-
-const divider = document.querySelector('.comments-section__guestbook-divider');
-
-function function1() {
+window.onload = parentFunction(); 
+    
+/*These functions create the comment sections, using data from the array*/
+    
+function loadComment() {
+    comments.forEach((data) => commentCreator(data));
+};
+    
+function commentCreator(comment) {
     let divider = document.createElement('div');
     divider.classList.add('divider', 'comments-section__guestbook-divider');
     let guestbookContainer = document.createElement('div');
@@ -35,29 +63,22 @@ function function1() {
     guestbookEntry.classList.add('comments-section__guestbook-entry');
     let guestbookName = document.createElement('h4');
     guestbookName.classList.add('comments-section__guestbook-name');
-    guestbookName.innerHTML = names[0];
     let guestbookDate = document.createElement('div');
     guestbookDate.classList.add('comments-section__guestbook-date');
-    guestbookDate.innerHTML = dates[0];
     let guestbookText = document.createElement('div');
     guestbookText.classList.add('comments-section__guestbook-text');
-    guestbookText.innerHTML = comments[0];
 
-    divider.insertBefore(guestbookContainer, null);
+    let guestbook = document.querySelector('.comments-section__guestbook');
+    
+    guestbook.appendChild(guestbookContainer);
     guestbookContainer.appendChild(guestbookPhoto);
     guestbookContainer.appendChild(guestbookEntry);
     guestbookEntry.appendChild(guestbookName);
     guestbookEntry.appendChild(guestbookDate);
     guestbookEntry.appendChild(guestbookText);
     guestbook.appendChild(divider);
+
+    guestbookName.innerText = comment.name;
+    guestbookDate.innerText = comment.date;
+    guestbookText.innerText = comment.comment;
 }
-
-setTimeout(function1, 0);
-
-function function2 () {
-    for (let i = 0; i < 2; i++) {
-        function1(i)
-    }
-}
-
-setTimeout(function2, 0);
--- Sandy-Nguyen-sprint-two/scripts/shows.js
+++ Sandy-Nguyen-sprint-two/scripts/shows.js
@@ -1,15 +1,50 @@
-const showsContainer = document.querySelector('.main-section__shows-container');
-const dates = ["Mon Dec 17 2018", "Tue Jul 18 2019", "Fri Jul 22 2019", "Sat Aug 12 2019", "Fri Sep 05 2019", "Wed Aug 11 2019"];
-const titles = ["DATE", "VENUE", "LOCATION"];
-const venues = ["Ronald Lane", "Pier 3 East", "View Lounge", "Hyatt Agency", "Moscow Center"];
-const locations = ["San Francisco, CA"];
+const shows = [{
+    date: 'Mon Dec 17 2018',
+    venue: 'Ronald Lane',
+    location: 'San Francisco, CA'
+}, {
+    date: 'Tue Jul 18 2019',
+    venue: 'Pier 3 East',
+    location: 'San Francisco, CA'
+}, {
+    date: 'Fri Jul 22 2019',
+    venue: 'View Lounge',
+    location: 'San Francisco, CA'
+}, {
+    date: 'Sat Aug 12 2019',
+    venue: 'Hyatt Agency',
+    location: 'San Francisco, CA'
+}, {
+    date: 'Fri Sep 05 2019',
+    venue: 'Moscow Center',
+    location: 'San Francisco, CA'
+}, {
+    date: 'Wed Aug 11 2019',
+    venue: 'Pres Club',
+    location: 'San Francisco, CA'
+}]
 
-function function1() {
+const titles = ["DATE", "VENUE", "LOCATION"];
+
+
+function parentFunction() {
+    let parentElement = document.createElement('div');
+    parentElement.classList.add('main-section__shows-container');
+    let parentParent = document.querySelector('.main-section');
+    parentParent.appendChild(parentElement);
+    
+    headings();
+    loadData();
+};
+
+window.onload = parentFunction();
+
+function headings() {
     let heading = document.createElement('div');
     heading.classList.add('main-section__shows-headings');
     let headingsTitleDate = document.createElement('p');
     headingsTitleDate.classList.add('main-section__shows-headings-title',  'main-section__shows-headings-title-date');
-    headingsTitleDate.innerHTML = titles[0]
+    headingsTitleDate.innerHTML = titles[0];
     let headingsTitleVenue = document.createElement('p');
     headingsTitleVenue.classList.add('main-section__shows-headings-title',  'main-section__shows-headings-title-venue');
     headingsTitleVenue.innerHTML = titles[1];
@@ -17,58 +52,68 @@ function function1() {
     headingsTitleLocation.classList.add('main-section__shows-headings-title',  'main-section__shows-headings-title-location');
     headingsTitleLocation.innerHTML = titles[2];
 
+    let showsContainer = document.querySelector('.main-section__shows-container');
+    
     showsContainer.appendChild(heading);
     heading.appendChild(headingsTitleDate);
     heading.appendChild(headingsTitleVenue);
     heading.appendChild(headingsTitleLocation);
+    
 }
 
-function function2() {
-    let shows = document.createElement('div');
-    shows.classList.add('main-section__shows');
+function loadData () {
+    shows.forEach((data) => showCreator(data));
+}
+
+function showCreator(show) {
+    let showsList = document.createElement('div');
+    showsList.classList.add('main-section__shows');
     let showsTitle1 = document.createElement('p');
     showsTitle1.classList.add('main-section__shows-content', 'main-section__shows-title');
-    showsTitle1.innerHTML = titles[0];
     let showsDate = document.createElement('p');
     showsDate.classList.add('main-section__shows-content', 'main-section__shows-date');
-    showsDate.innerHTML = dates[0];
     let showsTitle2 = document.createElement('p');
     showsTitle2.classList.add('main-section__shows-content', 'main-section__shows-title');
-    showsTitle2.innerHTML = titles[0];
     let showsVenue = document.createElement('p');
     showsVenue.classList.add('main-section__shows-content', 'main-section__shows-venue');
-    showsVenue.innerHTML = venues[0];
     let showsTitle3 = document.createElement('p');
     showsTitle3.classList.add('main-section__shows-content', 'main-section__shows-title');
-    showsTitle3.innerHTML = titles[0];
     let showsLocation = document.createElement('p');
     showsLocation.classList.add('main-section__shows-content', 'main-section__shows-location');
-    showsLocation.innerHTML = locations[0];
     let button = document.createElement('button');
     button.classList.add('button', 'main-section__shows-button');
-    button.innerHTML = "BUY TICKETS";
+    button.innerText = "BUY TICKETS";
     let divider = document.createElement('div');
     divider.classList.add('divider', 'main-section__shows-divider');
 
-    showsContainer.appendChild(shows);
-    shows.appendChild(showsTitle1);
-    shows.appendChild(showsDate);
-    shows.appendChild(showsTitle2);
-    shows.appendChild(showsVenue);
-    shows.appendChild(showsTitle3);
-    shows.appendChild(showsLocation);
-    shows.appendChild(button);
-    shows.appendChild(divider);
+    let showsContainer = document.querySelector('.main-section__shows-container');
+
+    showsContainer.appendChild(showsList);
+    showsList.appendChild(showsTitle1);
+    showsList.appendChild(showsDate);
+    showsList.appendChild(showsTitle2);
+    showsList.appendChild(showsVenue);
+    showsList.appendChild(showsTitle3);
+    showsList.appendChild(showsLocation);
+    showsList.appendChild(button);
+    showsList.appendChild(divider);
+
+    showsTitle1.innerText = titles[0];
+    showsDate.innerText = show.date;
+    showsTitle2.innerText = titles[1];
+    showsVenue.innerText = show.venue;
+    showsTitle3.innerText = titles[2];
+    showsLocation.innerText = show.location;
 }
 
-setTimeout(function1, 0);
+// setTimeout(function1, 0);
 
-setTimeout(function2, 0)
+// setTimeout(showCreator, 0)
 
-function function3() {
-    for (let i = 0; i < 5; i++) {
-        function2(i);
-    }
-}
+// function function4() {
+// for (let i = 0; i < 5; i++) {
+//         showCreator(i);
+//     }
+// }
 
-setTimeout(function3, 0);
+// setTimeout(function4, 0)
